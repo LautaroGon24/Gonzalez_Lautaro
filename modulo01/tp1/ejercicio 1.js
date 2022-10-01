c598a489ffdfb5726193ce56a89dc333739d55ee
González Lautaro Ezequiel <114779722+LautaroGon24@users.noreply.github.com>
2022-10-01 20:10:02 -0300

    Creación ejercicio 1
    Creación ejercicio 1.

---
+++ modulo01/tp1/ejercicio 1.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Ejercicio 1 </title>
+</head>
+<body>
+<script>
+document.writeln("Hola Mundo!"); //imprime en pantalla "Hola Mundo!"
+document.writeln("Soy el primer script"); //imprime en pantalla "Soy el primer script"
+</script>
+</body>
+</html>

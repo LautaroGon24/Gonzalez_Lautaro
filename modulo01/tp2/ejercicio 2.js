0a51018661ef679089725d46eb607ffd37593e28
González Lautaro Ezequiel <114779722+LautaroGon24@users.noreply.github.com>
2022-10-01 21:39:47 -0300

    Creación de ejercicio 2
    Creación de ejercicio 2.

---
+++ modulo01/tp2/ejercicio 2.js
@@ -0,0 +1,16 @@
+//EJERCICIO 2
+var num1 = 3;
+var num2 = 7;
+if(num1 < num2) {
+console.log("num1 no es mayor que num2");
+}
+if(num2 >= 0) {
+console.log("num2 es positivo");
+}
+if(num1< 0 ) {
+console.log("num1 es negativo o distinto de cero");
+}
+if(num1 <= num2) {
+num1=++
+console.log("Incrementar el valor en 1 de num 1 no lo hace mayor o igual que num2");
+}  
